3ad3c218b46d9d1576695bb2087ad3ef0c3c6672
Benjamin <bnjackson951@gmail.com>
2023-05-22 19:08:33 +0100

    Complete other versions of exercise

--- 01-say-hello/say-hello.js
+++ 01-say-hello/say-hello.js
@@ -1,2 +1,16 @@
+//Version 1
+
 const usersName = prompt('What is your name?');
 console.log(`Hello, ${usersName}, nice to meet you!`);
+
+//version 2 - without using variables
+
+console.log(`Hello, ${prompt('What is your name?')}, nice to meet you!`);
+
+//Version 3 - a version with different repsonses for different names 
+
+function outputNameLength(name) {
+    return `Your names length is ${name.length} letters`;
+}
+
+console.log(outputNameLength(usersName));
