09c5b8bc7a15f3d7c7d0243ea59308b552c195ae
Benjamin <bnjackson951@gmail.com>
2023-06-27 14:59:50 +0100

    Complete exercise: add functions to calculate users BMI, aswell as users weight class, and output the results

--- 19-bmi-calculator/bmi-calculator.js
+++ 19-bmi-calculator/bmi-calculator.js
@@ -8,15 +8,16 @@ function getUserChoice() {
         getImperialMeasurements();
     } else {
         console.log('Please enter either "Imperial" or "Metric"');
+        getUserChoice();
     }
     function getMetricMeasurements() {
-        const usersHeight = Number(readlineSync.question('What is youre height in cm? '));
-        const usersWeight = Number(readlineSync.question('What is youre weight in kg? '));
+        const usersHeight = Number(readlineSync.question('What is your height in cm? '));
+        const usersWeight = Number(readlineSync.question('What is your weight in kg? '));
         checkUserMeasurements(usersHeight, usersWeight, 'metric');
     } 
     function getImperialMeasurements() {
-        const usersHeight = Number(readlineSync.question('What is youre height in inches? '));
-        const usersWeight = Number(readlineSync.question('What is youre weight in pounds? '));
+        const usersHeight = Number(readlineSync.question('What is your height in inches? '));
+        const usersWeight = Number(readlineSync.question('What is your weight in pounds? '));
         checkUserMeasurements(usersHeight, usersWeight, 'imperial');
     }
     function checkUserMeasurements(usersHeight, usersWeight, system) {
@@ -29,8 +30,33 @@ function getUserChoice() {
     }
 }
 
-function calcUsersBMI() {
-
+function calcUsersBMI(height, weight, system) {
+    let BMI;
+    let usersWeightClass;
+    if(system === 'metric') {
+        BMI = (weight / height / height) * 10000;
+    } else {
+        BMI = weight / (height * height) * 703;
+    }
+    function calcUserWeightClass() {
+        if (BMI < 18.5) {
+            usersWeightClass = 'Underweight';
+        } else if (BMI >= 18.5 && BMI <= 24.9) {
+            usersWeightClass = 'Normal';
+        } else if (BMI >= 25 && BMI <= 29.9) {
+            usersWeightClass = 'Overweight';
+        } else if (BMI >= 30 && BMI <= 34.9) {
+            usersWeightClass = 'Obese';
+        } else if (BMI > 35) {
+            usersWeightClass = 'Extemely Obese';
+        }
+    }
+    calcUserWeightClass();
+    function outputResults() {
+        BMI = BMI.toFixed(2);
+        console.log(`Your BMI is ${BMI}, and your weight class is ${usersWeightClass}.`);
+    }
+    outputResults();
 }
 
 getUserChoice();
